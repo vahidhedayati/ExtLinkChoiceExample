476e9b9cd3eea1bfc8e3c83773072aa5855a9ccf
Vahid H <vahid.hedayati@gmail.com>
2013-09-11 21:09:09 +0100

    Update BuildConfig.groovy

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -59,6 +59,6 @@ grails.project.dependency.resolution = {
         runtime ":database-migration:1.3.2"
 
         compile ':cache:1.0.1'
-		compile ":extlinkchoice:0.2"
+		compile ":extlinkchoice:0.3"
     }
 }
