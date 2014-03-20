3e9350b553b99e2fbb3d3c6b6486ba35a9c90e64
Vahid <vahid.hedayati@gmail.com>
2014-03-20 22:01:25 +0000

    .

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -68,8 +68,8 @@ grails.project.dependency.resolution = {
         //runtime ":zipped-resources:1.0.1"
         //runtime ":cached-resources:1.1"
         //runtime ":yui-minify-resources:0.1.5"
-		//compile ':extlinkchoice:0.10'
+		compile ':extlinkchoice:0.10'
 		
     }
 }
-grails.plugin.location.'extlinkchoice' = "../ExtLinkChoice"
+//grails.plugin.location.'extlinkchoice' = "../ExtLinkChoice"
